831833c1184fe9bdf48615a4adeade05dd98a107
Antonio André <antonioandre1008@gmail.com>
2024-08-01 07:53:09 -0500

    up

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router/auto'
+
+</script>
+
+<template>
+  <RouterView/>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1 class="text-4xl">Olá</h1>
+</template>
+
+<script setup>
+
+</script>
+
+
