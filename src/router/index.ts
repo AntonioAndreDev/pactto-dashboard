43397c39394f9a766ec36f812f620fb3e9a4bc14
Antonio André <antonioandre1008@gmail.com>
2024-08-01 08:41:59 -0500

    style: estiliza linkExactActiveClass

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import {routes} from 'vue-router/auto-routes'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    linkExactActiveClass: 'dark:text-black dark:bg-custom-primary-yellow',
     routes,
 })
 
