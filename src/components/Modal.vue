c26938cdec74f337737d7e329f58eefd76c00ef1
Antonio André <antonioandre1008@gmail.com>
2024-08-02 10:22:51 -0500

    feat: inclui modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-	<TransitionRoot as="template" :show="open">
-		<Dialog class="relative z-10" @close="open = false">
+	<TransitionRoot as="template" :show="isOpen">
+		<Dialog class="relative z-10" @close="closeModal">
 			<TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
 							 leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
 				<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
@@ -23,12 +23,10 @@
 									</div>
 									<div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
 										<DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">
-											Deactivate account
+											{{ title }}
 										</DialogTitle>
 										<div class="mt-2">
-											<p class="text-sm text-gray-500">Are you sure you want to deactivate your
-												account? All of your data will be permanently removed. This action
-												cannot be undone.</p>
+											<p class="text-sm text-gray-500">{{ description }}.</p>
 										</div>
 									</div>
 								</div>
@@ -36,11 +34,11 @@
 							<div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
 								<button type="button"
 										class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-										@click="open = false">Deactivate
+										@click="action">{{ mainBtnText }}
 								</button>
 								<button type="button"
 										class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-										@click="open = false" ref="cancelButtonRef">Cancel
+										@click="closeModal" ref="cancelButtonRef">{{ subBtnText }}
 								</button>
 							</div>
 						</DialogPanel>
@@ -51,10 +49,25 @@
 	</TransitionRoot>
 </template>
 
-<script setup>
-import {ref} from 'vue'
+<script setup lang="ts">
 import {Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot} from '@headlessui/vue'
 import {ExclamationTriangleIcon} from '@heroicons/vue/24/outline'
 
-const open = ref(true)
+defineProps({
+	isOpen: Boolean,
+	mainBtnText: String,
+	subBtnText: String,
+	title: String,
+	description: String
+})
+
+const emits = defineEmits(['update:isOpen', 'action'])
+
+const closeModal = () => {
+	emits('update:isOpen', false)
+}
+
+const action = () => {
+	emits('action')
+}
 </script>
